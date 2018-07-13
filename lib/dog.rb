72d0d1b9be1badf40c2d6745aeace3fc5bb59d2c
KristellaBui <kristellabui@gmail.com>
2018-07-13 13:53:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,9 @@
-# dog.rb
-
-lassie = Dog.new
-lassie.name = "@Lassie"
+class Dog
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end
  
-lassie.name #=> "Lassie"
-
+  def name
+    this_dogs_name
+  end
+end
